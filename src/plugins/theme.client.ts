6a350f767316804613c7f8579b07af153601f50f
Kikyo1337 <9478956@qq.com>
2024-06-18 17:03:43 +0800

    Update theme.client.ts

--- src/plugins/theme.client.ts
+++ src/plugins/theme.client.ts
@@ -1,7 +1,7 @@
 /*
- * @Author: dushuai
+ * @Author: Steam Game
  * @Date: 2023-12-10 20:49:41
- * @LastEditors: dushuai
+ * @LastEditors: Steam Game
  * @LastEditTime: 2024-02-21 16:45:38
  * @Description: 描述
  */
