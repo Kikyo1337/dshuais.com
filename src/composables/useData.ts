d4a1a6ad763613838fdad41c58dc5fc8c7039db3
Kikyo1337 <9478956@qq.com>
2024-06-18 17:18:50 +0800

    Update useData.ts

--- src/composables/useData.ts
+++ src/composables/useData.ts
@@ -1,5 +1,5 @@
 /*
- * @Author: dushuai
+ * @Author: Steam Game
  * @Date: 2024-03-11 14:40:06
  * @LastEditors: dushuai
  * @LastEditTime: 2024-03-11 16:04:34
@@ -18,11 +18,11 @@ export const useData = () => {
    * 网站列表
    */
   const websites = ref<Website[]>([
-    { name: 'homepage', href: 'https://homepage.dshuais.com/', icon: 'mdi:web-check' },
-    { name: '博客', href: 'https://blog.dshuais.com/', icon: 'fa6-solid:blog' },
+    { name: '辅助下载', href: 'https://www.notion.so/Steam-Cheat-b81ebe5c8e684c3bba70c911f2c6fbc3', icon: 'mdi:web-check' },
+    { name: '查询Ｑ绑', href: 'https://zy.xywlapi.cc/home.html', icon: 'fa6-solid:blog' },
     // { name: 'create-kd', href: 'https://create-keep-design.dshuais.com/', icon: 'k' },
-    { name: 'keepdesign', href: 'https://keep-design.dshuais.com/', icon: 'https://files.dshuais.com/images/logo/k.png', iconType: 'img' },
-    { name: 'danmaku', href: 'https://danmaku-vue.dshuais.com/', icon: 'https://files.dshuais.com/images/logo/dm.png', iconType: 'img' },
+    { name: '其他软件', href: 'https://www.notion.so/e4532fd40e544508b3c9061eb0991ff3?pvs=4', icon: 'https://files.dshuais.com/images/logo/k.png', iconType: 'img' },
+    { name: '联系方式', href: 'https://www.notion.so/3ff1c5c5efb04b40b696a199271ed415?pvs=4', icon: 'https://files.dshuais.com/images/logo/dm.png', iconType: 'img' },
   ])
 
   return {
