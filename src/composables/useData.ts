2d13fed6c1737606c54f851b5e0b3a9019b3f942
Kikyo1337 <9478956@qq.com>
2024-06-18 17:21:15 +0800

    Update useData.ts

--- src/composables/useData.ts
+++ src/composables/useData.ts
@@ -19,9 +19,9 @@ export const useData = () => {
    */
   const websites = ref<Website[]>([
     { name: '辅助下载', href: 'https://www.notion.so/Steam-Cheat-b81ebe5c8e684c3bba70c911f2c6fbc3', icon: 'mdi:web-check' },
-    { name: '查询Ｑ绑', href: 'https://zy.xywlapi.cc/home.html', icon: 'fa6-solid:blog' },
+    { name: '查询Ｑ绑', href: 'https://zy.xywlapi.cc/home.html', icon: 'mdi:web-check' },
     // { name: 'create-kd', href: 'https://create-keep-design.dshuais.com/', icon: 'k' },
-    { name: '其他软件', href: 'https://www.notion.so/e4532fd40e544508b3c9061eb0991ff3?pvs=4', icon: 'https://files.dshuais.com/images/logo/k.png', iconType: 'img' },
+    { name: '其他软件', href: 'https://www.notion.so/e4532fd40e544508b3c9061eb0991ff3?pvs=4', icon: 'mdi:web-check' },
     { name: '联系方式', href: 'https://www.notion.so/3ff1c5c5efb04b40b696a199271ed415?pvs=4', icon: 'https://files.dshuais.com/images/logo/dm.png', iconType: 'img' },
   ])
 
